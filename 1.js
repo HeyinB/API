2632661ca4f138b103ac6e050f1f5d32a4526942
“hyb” <3164249018@qq.com>
2022-08-04 00:07:25 +0800

    配置babel

---
+++ 1.js
@@ -0,0 +1,13 @@
+
+
+let a = `npm uninstall babel-cli  babel-preset-env koa-passport nodemon passport-jwt --save-dev `
+
+let b = `npm install babel-cli  babel-preset-env koa-passport nodemon passport-jwt --save `
+
+
+
+let all = `"babel-cli": "^6.26.0",
+"babel-preset-env": "^1.7.0",
+"koa-passport": "^4.1.4",
+"nodemon": "^2.0.15",
+"passport-jwt": "^4.0.0"`
--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser'
 import passport from 'koa-passport'
 import cors from 'koa2-cors'
+import { ScretKeys } from './config/config'
+import jwt from "jsonwebtoken";
+
 
 let app = new Koa();
 app.use(bodyParser());
@@ -13,14 +16,45 @@ app.use(passport.initialize())
 app.use(passport.session())
 require('./util/passport')(passport)
 
-app.use((ctx, next) => {
-    console.log(ctx.request.url)
-    ctx.body = {
-        code: 200,
-        msg: '连接上了'
-    }
-}
-)
+app.use(function (ctx, next) {
+    let pass = ['/v1/user/LoginOrRegister', '/v1/user/refreshToken']
+    let url = ctx.request.url, AllToken = ctx.request.header.authorization
+
+    console.log('----------------', url);
+    // if (!pass.includes(url) && AllToken) {
+    //     console.log('-------ctx.request---------进来了');
+    //     const parts = AllToken.split(' ');
+    //     let statu = jwt.verify(parts[1], ScretKeys, (err, decoded) => {
+    //         if (err) {
+    //             console.log(err.name)
+    //             let res = {}
+    //             switch (err.name) {
+    //                 case 'JsonWebTokenError':
+    //                     console.log('无效的token')
+    //                     // res.status(403).send({ code: -1, msg: '无效的token' });
+    //                     res = {
+    //                         code: 403,
+    //                         msg: '无效的token'
+    //                     }
+    //                     break;
+    //                 case 'TokenExpiredError':
+    //                     // res.status(403).send({ code: -1, msg: 'token过期' });
+    //                     res = {
+    //                         code: 403,
+    //                         msg: 'token过期'
+    //                     }
+    //                     break;
+    //             }
+    //             return res
+    //         }
+    //     })
+    //     ctx.body = statu
+    // } else {
+    //     next()
+    // }
+
+
+});
 
 const res = readFile(process.cwd() + '/router')
 const mainRouter = new Router()
