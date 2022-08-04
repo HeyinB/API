215235f71b1d4b04d292575751de3b04cbdd488d
HeyinB <3164249018@qq.com>
2022-08-04 17:33:17 +0800

    修改初始配置

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 
 import { readFile } from './util'
+import { authToken } from './util/token'
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser'
 import passport from 'koa-passport'
 import cors from 'koa2-cors'
-import { ScretKeys } from './config/config'
-import jwt from "jsonwebtoken";
-
 
 let app = new Koa();
 app.use(bodyParser());
@@ -16,44 +14,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 require('./util/passport')(passport)
 
-app.use(function (ctx, next) {
-    let pass = ['/v1/user/LoginOrRegister', '/v1/user/refreshToken']
-    let url = ctx.request.url, AllToken = ctx.request.header.authorization
-
-    console.log('----------------', url);
-    // if (!pass.includes(url) && AllToken) {
-    //     console.log('-------ctx.request---------进来了');
-    //     const parts = AllToken.split(' ');
-    //     let statu = jwt.verify(parts[1], ScretKeys, (err, decoded) => {
-    //         if (err) {
-    //             console.log(err.name)
-    //             let res = {}
-    //             switch (err.name) {
-    //                 case 'JsonWebTokenError':
-    //                     console.log('无效的token')
-    //                     // res.status(403).send({ code: -1, msg: '无效的token' });
-    //                     res = {
-    //                         code: 403,
-    //                         msg: '无效的token'
-    //                     }
-    //                     break;
-    //                 case 'TokenExpiredError':
-    //                     // res.status(403).send({ code: -1, msg: 'token过期' });
-    //                     res = {
-    //                         code: 403,
-    //                         msg: 'token过期'
-    //                     }
-    //                     break;
-    //             }
-    //             return res
-    //         }
-    //     })
-    //     ctx.body = statu
-    // } else {
-    //     next()
-    // }
-
-
+app.use((ctx, next) => {
+    authToken(ctx, next)
 });
 
 const res = readFile(process.cwd() + '/router')
--- util/token.js
+++ util/token.js
@@ -10,7 +10,51 @@ const getToken = function (payLoad) {
   return jwt.sign(payLoad, ScretKeys, { expiresIn: Time });
 }
 
+const authToken = async function (ctx, next) {
+  let pass = ['/v1/user/LoginOrRegister', '/v1/user/refreshToken']
+  let url = ctx.request.url, AllToken = ctx.request.header.authorization
+
+  console.log('----------------', url);
+  if (!pass.includes(url) && AllToken) {
+    console.log('-------ctx.request---------进来了');
+    const parts = AllToken.split(' ');
+    let statu = jwt.verify(parts[1], ScretKeys, async (err, decoded) => {
+      if (err) {
+        console.log(err.name)
+        let res = {}
+        switch (err.name) {
+          case 'JsonWebTokenError':
+            console.log('无效的token')
+            // res.status(403).send({ code: -1, msg: '无效的token' });
+            res = {
+              code: 403,
+              msg: '无效的token'
+            }
+            break;
+          case 'TokenExpiredError':
+            // res.status(403).send({ code: -1, msg: 'token过期' });
+            res = {
+              code: 403,
+              msg: 'token过期'
+            }
+            break;
+        }
+        return res
+      } else {
+        await next()
+      }
+    })
+    ctx.body = statu
+  } else {
+    ctx.body = {
+      code: 404,
+      msg: '未找到'
+    }
+  }
+}
+
 
 module.exports = {
-  getToken
+  getToken,
+  authToken
 }
